d1b71cbc4fa1d685ba26eba3bca42784fc42a9f3
supersubwoofer@gmail.com <supersubwoofer@gmail.com>
2018-04-09 11:30:03 -0400

    mod route

--- lib/my_app_web/router.ex
+++ lib/my_app_web/router.ex
@@ -22,6 +22,7 @@ defmodule MyAppWeb.Router do
   scope "/api", MyAppWeb do
     pipe_through :api
 
-    resources "/users", UserController, except: [:new, :edit]
+    resources "/users", UserController, only: [:index]
+    resources "/users", UserController, only: [:show]
   end
 end
