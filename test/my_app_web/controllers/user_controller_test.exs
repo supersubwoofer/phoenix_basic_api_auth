e2a437de7ee6e09416fa35d3d8389fde04c2f472
supersubwoofer@gmail.com <supersubwoofer@gmail.com>
2018-04-29 21:39:53 -0400

    add tests for unauthorizied GET  api/users

--- test/my_app_web/controllers/user_controller_test.exs
+++ test/my_app_web/controllers/user_controller_test.exs
@@ -3,7 +3,18 @@ defmodule MyAppWeb.UserControllerTest do
   import MyApp.Factory
   import MyApp.Accounts.Guardian
   
-  test "#index renders a list of users", %{conn: conn} do
+  test "GET /api/users/ without permission", %{conn: conn} do
+    user = insert(:user)
+    {:ok, token, _} = encode_and_sign(user, %{}, permissions:  %{default: []})
+
+    conn = conn
+    |> put_req_header("authorization", "bearer #{token}")
+    |> get(user_path(conn, :index))
+
+    assert json_response(conn, 401) == %{"message" => "unauthorized"}
+  end
+
+  test "GET /api/users/ renders a list of users", %{conn: conn} do
     user = insert(:user)
     {:ok, token, _} = encode_and_sign(user, %{}, permissions: user.permissions)
 
@@ -14,7 +25,18 @@ defmodule MyAppWeb.UserControllerTest do
     assert json_response(conn, 200) == render_json("index.json", users: [user])
   end
 
-  test "#show renders a single user", %{conn: conn} do
+  test "GET /api/users/:show without permission", %{conn: conn} do
+    user = insert(:user)
+    {:ok, token, _} = encode_and_sign(user, %{}, permissions: %{default: []})
+
+    conn = conn
+    |> put_req_header("authorization", "bearer #{token}")
+    |> get(user_path(conn, :show, user.email))
+
+    assert json_response(conn, 401) == %{"message" => "unauthorized"}
+  end
+
+  test "GET /api/users/:show renders a single user", %{conn: conn} do
     user = insert(:user)
     {:ok, token, _} = encode_and_sign(user, %{}, permissions: user.permissions)
 
